60f0387ab25f0501b61e8c783f0dfae5d7225635
youpinyao <497400448@qq.com>
2020-06-12 10:17:02 +0800

    refactor

--- src/init.js
+++ src/init.js
@@ -1,12 +1,11 @@
 
 const fs = require('fs');
-const os = require('os');
 const path = require('path');
 
 const configName = require('./configName');
 
 function init() {
-  fs.copyFileSync(path.resolve(__dirname, configName), path.resolve(os.homedir(), configName));
+  fs.copyFileSync(path.resolve(__dirname, configName), path.resolve(process.cwd(), configName));
 }
 
 module.exports = init;
